c3f831e176b14912cfc4f716e05506d4f80499b6
Thang Tran <thuutran@amperecomputing.com>
2024-12-19 15:52:57 +0700

    meta-ampere: flash: update the method to turn on the Host after flashing
    When BMC is flashing firmware, the ActivateBlockTransiton interface is
    added. The PSM checks the ActivateBlockTransiton exits or not, if this
    interface exits then Transition request is rejected. Otherwise, we handles
    transition requests. Therefore, we have to check the ActivateBlockTransiton
    interface before requesting to turn the Host on after flashing.
    
    Tested:
        1. Turn the Host on.
        2. Request to flash new firmware via Redfish/WebUi.
        3. During flashing, Host is turned off.
        4. When flashing done. Host is turned on.
    
    Signed-off-by: Thang Tran <thuutran@amperecomputing.com>

--- meta-ampere/meta-common/recipes-phosphor/flash/phosphor-software-manager/turn_on_host_after_flash.sh
+++ meta-ampere/meta-common/recipes-phosphor/flash/phosphor-software-manager/turn_on_host_after_flash.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # This script is used to turn on the Host after flashing.
-# It has to wait for BMC's state enter to "Ready" status
-# due to current BMC's state is "UpdateInProgress", therefore
-# we can not request to turn on the Host immediately.
+# It has to wait until no more firmware being updated by checking
+# xyz.openbmc_project.Software.ActivationBlocksTransition interface.
 
 time_out=30
 
@@ -12,17 +11,29 @@ then
     time_out=$1
 fi
 
+function is_fw_updating()
+{
+    number_fw_is_updating=$(busctl call xyz.openbmc_project.ObjectMapper /xyz/openbmc_project/object_mapper \
+            xyz.openbmc_project.ObjectMapper GetSubTree sias / 0 1 \
+            xyz.openbmc_project.Software.ActivationBlocksTransition | cut -d " " -f 2)
+
+    if [ "$number_fw_is_updating" == "0" ]
+    then
+        echo "False"
+    else
+        echo "True"
+    fi
+}
+
 for i in $(seq 1 "$time_out")
 do
-    bmc_state=$(busctl get-property xyz.openbmc_project.State.BMC \
-                /xyz/openbmc_project/state/bmc0 xyz.openbmc_project.State.BMC \
-                CurrentBMCState | cut -d " " -f 2)
+    is_fw_updating=$(is_fw_updating)
 
-    if [[ "${bmc_state}" == *".Ready"* ]]
+    if [ "$is_fw_updating" == "False" ]
     then
         temp_val=$(busctl set-property xyz.openbmc_project.State.Host0 \
-                /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host \
-                RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.On)
+                    /xyz/openbmc_project/state/host0 xyz.openbmc_project.State.Host \
+                    RequestedHostTransition s xyz.openbmc_project.State.Host.Transition.On)
         break
     fi
 
