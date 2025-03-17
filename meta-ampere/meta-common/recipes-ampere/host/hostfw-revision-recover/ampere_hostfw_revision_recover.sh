fb855310be772a6d75c6405f6687678ab1b57a1b
Hieu Huynh <hieuh@os.amperecomputing.com>
2025-03-17 09:33:03 +0000

    meta-ampere: SetHostFWRev: update the service that includes firmware's version
    phosphor-software-manager switches to use xyz.openbmc_project.Software.Manager
    service to control software version/updating.
    This commit updates the name of service.
    
    Tested:
    1. Get Host FW via Redfish
       GET /redfish/v1/Systems/system
    2. Upgrade BMC FW via Webui/Redfish
    3. Get Host FW via Redfish, the Host FW version is the same step #1
    
    Signed-off-by: Hieu Huynh <hieuh@os.amperecomputing.com>

--- meta-ampere/meta-common/recipes-ampere/host/hostfw-revision-recover/ampere_hostfw_revision_recover.sh
+++ meta-ampere/meta-common/recipes-ampere/host/hostfw-revision-recover/ampere_hostfw_revision_recover.sh
@@ -5,7 +5,7 @@ HOST_FW=/var/lib/host_fw_revision
 # Update the Host Firmware Revision to D-bus
 if [ -f "$HOST_FW" ]; then
     host_fw=$(cat $HOST_FW)
-    busctl set-property xyz.openbmc_project.Software.BMC.Updater \
+    busctl set-property xyz.openbmc_project.Software.Manager \
         /xyz/openbmc_project/software/bios_active \
         xyz.openbmc_project.Software.Version Version s "$host_fw"
 fi
