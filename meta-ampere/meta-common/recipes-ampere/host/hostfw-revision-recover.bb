tinhnampere/openbmc-dev

meta-ampere: recover Host FW revision after BMC boots
Add new service to check for Host FW revision from
/var/lib/host_fw_revision which was stored by Ampere IPMI OEM Set Host
Firmware Revision command.

Tested:
1. Flash HostFW via WebUI.
2. Check HostFW display via Redfish
   GET /redfish/v1/UpdateService/FirmwareInventory/bios_active
   "Version": "03.02.00004001"
3. A/C or reboot BMC, check host fw is the same as #2.

Signed-off-by: Hieu Huynh <hieuh@os.amperecomputing.com>

meta-ampere/meta-common/recipes-ampere/host/hostfw-revision-recover.bb
@@ -1,0 +1,27 @@
+SUMMARY = "Phosphor OpenBMC Recover Host Firmware Revision Service"
+DESCRIPTION = "Phosphor OpenBMC Recover Host Firmware Revision Daemon"
+
+PR = "r1"
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/files/common-licenses/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
+
+inherit systemd
+inherit obmc-phosphor-systemd
+
+DEPENDS += "systemd"
+RDEPENDS:${PN} += "libsystemd"
+RDEPENDS:${PN} += "bash"
+
+SRC_URI = " \
+           file://ampere_hostfw_revision_recover.sh \
+           file://hostfw-revision-recover.service \
+          "
+
+SYSTEMD_PACKAGES = "${PN}"
+
+SYSTEMD_SERVICE:${PN} += "hostfw-revision-recover.service"
+
+do_install () {
+    install -d ${D}${sbindir}
+    install -m 0755 ${WORKDIR}/ampere_hostfw_revision_recover.sh ${D}${sbindir}/
+}

ampere-openbmc/openbmc

meta-ampere/meta-common/recipes-ampere/host/hostfw-revision-recover.bb
@@ -1,0 +1,30 @@
+SUMMARY = "Phosphor OpenBMC Recover Host Firmware Revision Service"
+DESCRIPTION = "Phosphor OpenBMC Recover Host Firmware Revision Daemon"
+
+PR = "r1"
+LICENSE = "Apache-2.0"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/files/common-licenses/Apache-2.0;md5=89aea4e17d99a7cacdbeed46a0096b10"
+
+S = "${WORKDIR}/sources"
+UNPACKDIR = "${S}"
+
+inherit systemd
+inherit obmc-phosphor-systemd
+
+DEPENDS += "systemd"
+RDEPENDS:${PN} += "libsystemd"
+RDEPENDS:${PN} += "bash"
+
+SRC_URI = " \
+           file://ampere_hostfw_revision_recover.sh \
+           file://hostfw-revision-recover.service \
+          "
+
+SYSTEMD_PACKAGES = "${PN}"
+
+SYSTEMD_SERVICE:${PN} += "hostfw-revision-recover.service"
+
+do_install () {
+    install -d ${D}${sbindir}
+    install -m 0755 ${UNPACKDIR}/ampere_hostfw_revision_recover.sh ${D}${sbindir}/
+}